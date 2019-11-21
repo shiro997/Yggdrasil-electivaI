aa52c61b6b44a4a82ccf20105029c72df6a56608
Diego Cortazar <cortazardiego@gmail.com>
2019-11-20 20:56:05 -0500

    Added Trackable

--- electivaI/Assets/Scripts/AudioProject.cs
+++ electivaI/Assets/Scripts/AudioProject.cs
@@ -3,12 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 using Vuforia;
 
-public class AudioProject : MonoBehaviour, IVirtualButtonEventHandler
+public class AudioProject : MonoBehaviour, IVirtualButtonEventHandler, ITrackableEventHandler
 {
 
     public AudioSource audioSource;
     public AudioClip[] audioClipArray;
     private GameObject repeat;
+    private TrackableBehaviour mTrackableBehaviour;
     AudioClip lastClip;
 
     void Start()
@@ -16,6 +17,12 @@ public class AudioProject : MonoBehaviour, IVirtualButtonEventHandler
         audioSource.PlayOneShot(RandomClip());
         repeat = GameObject.Find("repetir");
         repeat.GetComponent<VirtualButtonBehaviour>().RegisterEventHandler(this);
+        mTrackableBehaviour = GetComponent<TrackableBehaviour>();
+        if (mTrackableBehaviour)
+        {
+            mTrackableBehaviour.RegisterTrackableEventHandler(this
+);
+        }
     }
 
     AudioClip RandomClip()
@@ -33,6 +40,67 @@ public class AudioProject : MonoBehaviour, IVirtualButtonEventHandler
         return newClip;
     }
 
+    public void OnTrackableStateChanged(
+        TrackableBehaviour.Status previousStatus, TrackableBehaviour.Status newStatus)
+    {
+        if (newStatus == TrackableBehaviour.Status.DETECTED ||
+            newStatus == TrackableBehaviour.Status.TRACKED ||
+            newStatus == TrackableBehaviour.Status.EXTENDED_TRACKED)
+        {
+            OnTrackingFound();
+        }
+        else
+        {
+            OnTrackingLost();
+        }
+    }
+
+
+    private void OnTrackingFound()
+    {
+        Renderer[] rendererComponents = GetComponentsInChildren  <Renderer> (true);
+        Collider[] colliderComponents = GetComponentsInChildren <Collider>  (true);
+        // Enable rendering:
+        foreach (Renderer component in rendererComponents)
+        {
+            component.enabled = true;
+        }
+        // Enable colliders:
+        foreach (Collider component in colliderComponents)
+        {
+            component.enabled = true;
+        }
+        Debug.Log("Trackable " +mTrackableBehaviour.TrackableName+ " found");
+        if (mTrackableBehaviour.TrackableName == "manzana")
+            {
+            Debug.Log("Hello");
+        }
+        if (mTrackableBehaviour.TrackableName =="unitychan ")
+            {
+            //playSound(" sounds / audiosample ");
+        }
+
+    }
+
+    private void OnTrackingLost()
+    {
+        Renderer[] rendererComponents = GetComponentsInChildren<Renderer>(true);
+        Collider[] colliderComponents = GetComponentsInChildren<Collider>(true);
+        
+        foreach (Renderer component in rendererComponents)
+        {
+            component.enabled = false;
+        }
+
+        foreach (Collider component in colliderComponents)
+        {
+            component.enabled = false;
+        }
+        Debug.Log("Trackable" + mTrackableBehaviour.TrackableName + "lost");
+
+    }
+
+
     public void OnButtonPressed(VirtualButtonBehaviour vb)
     {
         audioSource.PlayOneShot(lastClip);
@@ -40,6 +108,6 @@ public class AudioProject : MonoBehaviour, IVirtualButtonEventHandler
 
     public void OnButtonReleased(VirtualButtonBehaviour vb)
     {
-        Debug.Log("liberado prro!!!!!");
+        Debug.Log("liberado!!!!!");
     }
 }
